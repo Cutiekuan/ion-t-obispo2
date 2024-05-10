66384ee6dd9336b2dc4aa9195c3a7d28314a85e1
Cutiekuan <miks05102002@gmail.com>
2024-05-10 09:09:44 +0800

    Updates

--- assets/md.transition-legacy-Bg2OA-ou.js
+++ assets/md.transition-legacy-Bg2OA-ou.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BtMtK35U.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-NUPmsh5L.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
