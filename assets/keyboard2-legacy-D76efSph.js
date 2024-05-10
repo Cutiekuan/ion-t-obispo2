1688963dc28e8db32e15a57d5616f8081e1a8fb7
Cutiekuan <miks05102002@gmail.com>
2024-05-10 09:26:01 +0800

    Updates

--- assets/keyboard2-legacy-D76efSph.js
+++ assets/keyboard2-legacy-D76efSph.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DOP8IDUn.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BactSUUy.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
