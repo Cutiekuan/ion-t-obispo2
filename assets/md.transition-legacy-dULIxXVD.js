cb803a0adbac3500bf1355ee3a0484fb99977873
Cutiekuan <miks05102002@gmail.com>
2024-05-10 09:41:22 +0800

    Updates

--- assets/md.transition-legacy-dULIxXVD.js
+++ assets/md.transition-legacy-dULIxXVD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CudB0DBw.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-BX3oJK3V.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-D73VmZJ2.js
+++ assets/swipe-back-legacy-D73VmZJ2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CudB0DBw.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BX3oJK3V.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
