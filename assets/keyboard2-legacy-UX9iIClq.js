510426329e25b7faa25e58ce588a9f4649aa81c4
Cutiekuan <miks05102002@gmail.com>
2024-05-10 09:05:11 +0800

    Updates

--- assets/keyboard2-legacy-UX9iIClq.js
+++ assets/keyboard2-legacy-UX9iIClq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CghOV0Yn.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BtMtK35U.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-S-1h0UlQ.js
+++ assets/swipe-back-legacy-S-1h0UlQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CghOV0Yn.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BtMtK35U.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
