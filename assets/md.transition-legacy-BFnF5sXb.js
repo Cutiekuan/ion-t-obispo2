487072775a469333f3f0c60f6d8e5d243dd83e7f
Cutiekuan <miks05102002@gmail.com>
2024-05-10 09:14:07 +0800

    Updates

--- assets/md.transition-legacy-BFnF5sXb.js
+++ assets/md.transition-legacy-BFnF5sXb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-NUPmsh5L.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-DOP8IDUn.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
