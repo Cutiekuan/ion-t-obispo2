643a70cf81207793040d49458a06c4ea21e82a91
Cutiekuan <miks05102002@gmail.com>
2024-05-10 09:01:22 +0800

    Updates

--- assets/md.transition-legacy-VqX-qlmO.js
+++ assets/md.transition-legacy-VqX-qlmO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DMqYJQiF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-CghOV0Yn.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
